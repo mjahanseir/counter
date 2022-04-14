c8a0bd402e3e823901eafdba24e2dcfdd4bf4b66
mjahanseir <mjahanseir@gmail.com>
2022-04-13 21:17:02 -0600

    counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,20 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 1
   }
   render() {
     return (
       <div>
-        <span> {this.state.count}</span>
+        <span> {this.formatCount()}</span>
         <button>Inremeent</button>
       </div>
 
     )
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
