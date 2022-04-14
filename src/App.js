1a28e8dda610698cd46ab5902a847c47dc0aef27
mjahanseir <mjahanseir@gmail.com>
2022-04-14 10:08:52 -0600

    some props and cost App added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Counter from "./components/counter";
 
-class App extends Component {
-  render() {
-    return <Counter />;
-  }
-}
+// class App extends Component {
+//   render() {
+//return <Counter />;
+// }
 
+const App = () => {
+  const [data] = useState([
+    { id: 1, name: "Foo", age: "20" },
+    { id: 2, name: "Bar", age: "30" },
+    { id: 3, name: "Baz", age: "40" },
+  ]);
+  return (
+    <table>
+      <tbody>
+        {data.map((person, i) => (
+          <TableRow key={i} tableData={person} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+const TableRow = ({ key, tableData }) => {
+  return (
+    <tr>
+      <td>id: {tableData.id}</td>
+      <td>name: {tableData.name}</td>
+      <td>age: {tableData.age}</td>
+    </tr>
+  );
+};
 export default App;
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,27 +2,24 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-  }
-
-  handleIncrement = pr => {
-    console.log(pr);
+    count: 0
+  };
+  handleIncrement = () => {
     this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement(pr)}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
-          Inremeent
+          Increment
         </button>
       </div>
-
-    )
+    );
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
