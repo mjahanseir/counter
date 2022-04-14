75b6ad94d999f49565c0df162164b546ba76fd08
mjahanseir <mjahanseir@gmail.com>
2022-04-13 21:08:40 -0600

    counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  }
   render() {
     return (
-     
-    );
+      <div>
+        <span> {this.state.count}</span>
+        <button>Inremeent</button>
+      </div>
+
+    )
   }
 }
+
 
 export default Counter;
